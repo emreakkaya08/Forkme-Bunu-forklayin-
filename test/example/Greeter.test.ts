825dca3bd680a96a0422bbbecfd93670e08c7c25
小e无双 <405284695@qq.com>
2023-04-10 20:23:51 +0800

    add testcase for greeter

--- test/example/Greeter.test.ts
+++ test/example/Greeter.test.ts
@@ -3,12 +3,27 @@ import { Contract } from 'ethers';
 import { ethers, upgrades } from 'hardhat';
 
 describe('Greeter', function () {
-  it("Should return the new greeting once it's changed", async function () {
-    let contract: Contract;
+  let contract: Contract;
+
+  beforeEach(async () => {
     const Greeter = await ethers.getContractFactory('Greeter');
     contract = await upgrades.deployProxy(Greeter, ['Hello, world!']);
     await contract.deployed();
+  });
 
+  it('Should get role to set greeting', async function () {
+    const [owner, signer2] = await ethers.getSigners();
+    await contract.connect(owner).setGreeting("it's ok!");
+
+    expect(await contract.greet()).to.equal("it's ok!");
+
+    const reason = `AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0xfdeede333c6261dc02ece53d289e60baf7bb77b797f7a4f0117b59fcca826981`;
+    await expect(
+      contract.connect(signer2).setGreeting("it's ok!")
+    ).to.revertedWith(reason);
+  });
+
+  it("Should return the new greeting once it's changed", async function () {
     expect(await contract.greet()).to.equal('Hello, world!');
 
     const [owner] = await ethers.getSigners();
@@ -24,5 +39,13 @@ describe('Greeter', function () {
     await setGreetingTx.wait();
 
     expect(await contract.greet()).to.equal('Hello, world!');
+  });
+
+  it('Should pause contract', async function () {
+    await contract.pause();
+
+    await expect(contract.setGreeting('Hola, mundo!')).to.be.revertedWith(
+      'Pausable: paused'
+    );
   });
 });
