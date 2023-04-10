a9a1a2b750d16fb2e07eb624152d6e32f67a620c
strawberry <liushuheng_ssss@163.com>
2023-04-10 15:03:55 +0800

    update ERC20 to ERC20Upgradeable
    add Upgradeable

--- contracts/token/XToken.sol
+++ contracts/token/XToken.sol
@@ -1,22 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract XToken is ERC20 {
+contract XToken is Initializable,AccessControlUpgradeable,UUPSUpgradeable,ERC20Upgradeable {
 
     //合约所有者
     address private contractOwner;
 
-    // 指定代币名称,符号
-    constructor() ERC20("XToken", "X") {
-        contractOwner = msg.sender;
-    }
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    // 初始化指定代币名称,符号,设置owner地址
+    function initialize(string memory name_, string memory symbol_, uint256 initialSupply) public virtual initializer { 
+         __ERC20_init(name_, symbol_); 
+         contractOwner = msg.sender;//指定合约所有者
+         __AccessControl_init();
+         __UUPSUpgradeable_init();
+         _mint(msg.sender, initialSupply);
+     }
 
     //USDT mint X
-    function mintWithUSDT(uint256 amount,address sender) public {
+    function mintWithUSDT(uint256 amount) public {
+        address sender = msg.sender;
         // USDT ERC20 token
-        ERC20 usdt = ERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+        ERC20Upgradeable usdt = ERC20Upgradeable(0xdAC17F958D2ee523a2206206994597C13D831ec7);
         // 检查调用者已经授权合约地址来管理足够数量的USDT代币
         uint256 allowance = usdt.allowance(sender, address(this));
         // 检查是否已批准此合同使用USDT
@@ -44,11 +54,13 @@ contract XToken is ERC20 {
 
     // 允许任何人检查合约所持有的任何ERC20令牌的余额
     function checkERC20Balance(address tokenAddress) public view returns (uint256) {
-        ERC20 token = ERC20(tokenAddress);
+        IERC20Upgradeable token = IERC20Upgradeable(tokenAddress);
         //获取合约所持有的令牌的余额
         uint256 balance = token.balanceOf(address(this));
         return balance;
     }
+    
+    // 指定更新权限
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
+
 }
-
-
