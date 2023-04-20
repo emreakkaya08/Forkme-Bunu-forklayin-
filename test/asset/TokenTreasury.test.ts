97dcde859cda6b6fe8c2fdde5f652f7791fb5aca
小e无双 <405284695@qq.com>
2023-04-20 15:29:00 +0800

    add snippet

--- test/asset/TokenTreasury.test.ts
+++ test/asset/TokenTreasury.test.ts
@@ -1,3 +1,19 @@
+import { expect } from 'chai';
+import { Contract } from 'ethers';
+import { ethers, upgrades } from 'hardhat';
+
 describe('TokenTreasury', async () => {
-  it('TokenTreasury Test', async function () {});
+  let contract: Contract;
+
+  beforeEach(async () => {
+    const TokenTreasuryContract = await ethers.getContractFactory(
+      'TokenTreasury'
+    );
+    contract = await upgrades.deployProxy(TokenTreasuryContract, []);
+    await contract.deployed();
+  });
+
+  it('TokenTreasury Test', async () => {
+    expect(contract).to.be.instanceOf(Contract);
+  });
 });
