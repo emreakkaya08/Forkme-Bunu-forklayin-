e4bd59af9338d80b835a1ff86524f6cba49c8444
小e无双 <405284695@qq.com>
2023-04-23 19:33:38 +0800

    deploy contracts

--- scripts/consts/deploy.address.const.ts
+++ scripts/consts/deploy.address.const.ts
@@ -24,13 +24,13 @@ const ContractDeployAddress_ETHTestNet: ContractDeployAddressInterface = {
   USDTFaucet: '0x861eC40cF915A2036617272a81ed16b339952E51',
   XToken: '0x9389023C56ed52f0Da18571500149d50430e7a1A',
   YToken: '0xa5bb1f61C8CFc133Bea27127565eB21B2C458CC3',
-  TokenCENO: null,
-  TokenZOIC: null,
+  TokenCENO: '0x858Bd29D36b5B62637963101258068892DDF49A7',
+  TokenZOIC: '0x7eC668de7E159564FA61B33617eD42a4522a6b86',
   TokenDeposit: '0xC077028f5396bd89f5FA1eB36772809974A51F10',
   TokenTreasury: '0xCA7BE8Ad675e77B8edF0D40ceE28bB4d3dC93E38',
   RedeemU: '0xaA71fe67B4c503ec5541661BBe16F9a03988019E',
 
-  TokenZOICCoffer: null,
+  TokenZOICCoffer: '0x199D69090b609175770920cEb056c65822F71529',
 };
 
 const ContractDeployAddress_ETHMainNet: ContractDeployAddressInterface = {};
