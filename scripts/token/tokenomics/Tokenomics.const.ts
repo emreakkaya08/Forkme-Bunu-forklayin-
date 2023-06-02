e15131334e6fade6001c565fb517b78bce069269
enixlee <405284695@qq.com>
2023-06-02 10:04:25 +0800

    deploy vesting

--- scripts/token/tokenomics/Tokenomics.const.ts
+++ scripts/token/tokenomics/Tokenomics.const.ts
@@ -22,7 +22,18 @@ const DAY = 24 * 60 * 60;
 export const A_WEEK = 7 * DAY;
 
 // 2023-07-01 00:00:00 UTC
-export const ZOIC_RELEASE_START_TIME = 1686115200;
+const ZOIC_RELEASE_START_TIME = 1686115200;
+
+export const ZoicReleaseStartTime = {
+  //TODO release start time for players vesting, it can be different from ZOIC_RELEASE_START_TIME
+  Players: ZOIC_RELEASE_START_TIME,
+  //TODO release start time for devs vesting, it can be different from ZOIC_RELEASE_START_TIME
+  Devs: ZOIC_RELEASE_START_TIME,
+  //TODO release start time for staking rewards vesting, it can be different from ZOIC_RELEASE_START_TIME
+  StakingRewards: ZOIC_RELEASE_START_TIME,
+  //TODO release start time for team vesting, it can be different from ZOIC_RELEASE_START_TIME
+  Team: ZOIC_RELEASE_START_TIME,
+};
 
 export const TOTAL_RELEASE_WEEKS = 360;
 
