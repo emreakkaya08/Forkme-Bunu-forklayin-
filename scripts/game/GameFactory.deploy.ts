ee840b1d42c16114835f06f2a1a82a2d8ac4b2c3
enixlee <405284695@qq.com>
2023-06-16 18:43:44 +0800

    modify

---
+++ scripts/game/GameFactory.deploy.ts
@@ -0,0 +1,27 @@
+import { ContractDeployAddress } from "../consts/deploy.address.const";
+import {
+  deployUpgradeProxy,
+  deployUpgradeUpdateWithProposal,
+} from "../utils/deploy.util";
+
+async function main() {
+  const contractAddress = ContractDeployAddress.GameFactory;
+  const DeployContractName = "GameFactory";
+  if (contractAddress) {
+    const contract = await deployUpgradeUpdateWithProposal(
+      DeployContractName,
+      contractAddress
+    );
+  } else {
+    const contract = await deployUpgradeProxy(DeployContractName, [
+      ContractDeployAddress.GameFactory,
+    ]);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
